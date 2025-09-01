7d4869e4f7a1413fb3b86476006a4b952990e64b
rholibobo <rholibobo@gmail.com>
2025-09-01 22:32:52 +0100

    updates

--- sanity.config.ts
+++ sanity.config.ts
@@ -4,7 +4,7 @@ import {structureTool} from 'sanity/structure'
 
 
 export default defineConfig({
-  name: 'FGCIK88set Data-House',
+  name: 'FGCIK88set_Data-House',
   title: 'FGCIK88',
   projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET!,
